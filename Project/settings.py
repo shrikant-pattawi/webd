d339d5d939a5fa80bcda7a7515fee92474806f3d
Sahaj-Bamba <sahajbamba1999@gmai.com>
2019-03-31 18:47:45 +0530

    updated some settings and added a test app for testing purposes

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_g$mq%_dg9lou50*60^)k)&6s3!d40z!n_s1_r6ky8belniucc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_own_test',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Calcutta'
 
 USE_I18N = True
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
---
+++ my_own_test/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyOwnTestConfig(AppConfig):
+    name = 'my_own_test'
