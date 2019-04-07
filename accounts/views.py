037e309a3578018c7a62c9ea1fd414e16235019a
Sahaj-Bamba <sahajbamba1999@gmai.com>
2019-04-08 01:29:39 +0530

    basic login signin signup logout done

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from django.contrib.auth import login,logout
 
 def user_signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('Project_Allotment_Portal:home')
+    else:
+        form = UserCreationForm()
     return render(request,'accounts/signup.html',{'form':form})
 
 def user_login(request):
-    return render(request,'accounts/login.html',{})
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect('Project_Allotment_Portal:home')
+    else :
+        form = AuthenticationForm()
+    return render(request,'accounts/login.html',{'form':form})
 
-
+def user_logout(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('Project_Allotment_Portal:home')
