27dabc46b385e2983e3bd82b247c51f820173a62
Shrikant <shrikantpattawi@gmail.com>
2019-04-13 12:42:56 +0530

    Script and login ,signup css

--- populate.py
+++ populate.py
@@ -7,6 +7,7 @@ from accounts.models import User_details
 
 def populate() :
     for i in range(1,5) :
+
         User_details.user_reg_no=i
         if i%2==0 :
             User_details.user_gender='M'
@@ -15,9 +16,9 @@ def populate() :
         User_details.user_contact=i*100000000
         User_details.user_cpi=i%8
         User_details.user_email="user"+str(i)+"@gmail.com"
-        User_details.user_name="user"+str(i)
+        User_details.user_name="user"+(i)
         User_details.data_verified=1
         User_details.authority=2
-        User_details.save(1)
+        User_details.save()
 
 populate()
