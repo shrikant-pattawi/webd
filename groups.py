29cf421ce992818daec3076425a20ae27d35e8b5
Sahaj Bamba <sahajbamba1999@gmail.com>
2019-04-18 02:28:07 +0530

    did work on scripts

---
+++ groups.py
@@ -0,0 +1,29 @@
+import random, sys, os, django
+import math
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","Project.settings")
+django.setup()
+
+from accounts.models import User_details
+from accounts.models import Team_details
+
+
+    # separate in groups
+
+n = len(User_details.objects.filter(data_verified=2).filter(authority__lte = 4))
+a = math.ceil(n/4)
+
+for i in range (1,5):
+    temp = User_details.objects.filter(data_verified=2).filter(authority__lte = 4).order_by('-user_cpi')[(i-1)*a:i*a]
+    for j in temp :
+        j.authority = 5-i
+        j.save()
+
+temp = User_details.objects.filter(data_verified=2).filter(authority__lte = 4).order_by('-user_cpi')[(0) * a:1 * a]
+for j in temp:
+    x = Team_details()
+    x.team_cpi = j.user_cpi
+    x.team_leader = j.user_user
+    x.team_name = j.user_name
+    x.save()
+
